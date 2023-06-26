ae1699e449b06ea4a294aff7f1b0b080459810ab
suica26 <g3122034e1@edu.teu.ac.jp>
2023-06-27 04:14:37 +0900

    プロジェクト作成

---
+++ Application/Application/Direct3D.cpp
@@ -0,0 +1,32 @@
+#include "Direct3D.h"
+
+#include <Windows.h>
+
+bool Direct3D::Initialize(HWND hWnd, int width, int height)
+{
+	ComPtr<IDXGIFactory> factory;
+
+	if (FAILED(CreateDXGIFactory(IID_PPV_ARGS(&factory))))
+	{
+		return false;
+	}
+
+	UINT creationFlags = 0;
+
+#ifdef _DEBUG
+	// DEBUGビルド時はDirect3Dのデバッグを有効にする
+	// (すごく重いが細かいエラーがわかる)
+	creationFlags != D3D11_CREATE_DEVICE_DEBUG;
+#endif
+
+	D3D_FEATURE_LEVEL featureLevels[] =
+	{
+		D3D_FEATURE_LEVEL_11_1,	// Direct3D 11.1	ShaderModel5
+		D3D_FEATURE_LEVEL_11_0,	// Direct3D 11		ShaderModel5
+		D3D_FEATURE_LEVEL_10_1,	// Direct3D 10.1	ShaderModel4
+		D3D_FEATURE_LEVEL_10_0,	// Direct3D 10.0	ShaderModel4
+		D3D_FEATURE_LEVEL_9_3,	// Direct3D 9.3		ShaderModel3
+		D3D_FEATURE_LEVEL_9_2,	// Direct3D 9.2		ShaderModel3
+		D3D_FEATURE_LEVEL_9_1,	// Direct3D 9.1		ShaderModel3
+	};
+}
